c9368908cd76612084a4dd56b1fb957346e06d31
VIPniuma <VIPniuma@users.noreply.github.com>
2026-09-14 22:40:46 +0800

    release: v0.3.41
    补充雷霆商店包介绍（安装器单文件/局内3选1/作者QQ与反馈群），同步修正README

--- src/HextechMod/Plugin.cs
+++ src/HextechMod/Plugin.cs
@@ -16,7 +16,7 @@ public sealed class HextechPlugin : BaseUnityPlugin
 {
     public const string Guid = "PeakModder.HextechMod";
     public const string Name = "HextechMod";
-    public const string Version = "0.3.40";
+    public const string Version = "0.3.41";
 
     internal static HextechPlugin Instance = null!;
     internal static ManualLogSource Log = null!;
